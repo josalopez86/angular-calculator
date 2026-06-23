c6177fc09c0e8491436c8608a675459eef8266ea
josalopez86 <josa.lopez@live.com.mx>
2026-06-23 11:59:28 -0700

    adding multiplication function

--- src/app/calculator/services/calculatorService.ts
+++ src/app/calculator/services/calculatorService.ts
@@ -1,7 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 
 const numbers = ["0","1","2","3","4","5","6","7","8","9"];
-const operators = ["/","*","-","+"];
+const operators = ["÷","x","-","+"];
 const specialOperators = ["C", "+/-","%",".","=", "Backspace"];
 
 
@@ -21,7 +21,7 @@ export class CalculatorService {
     }
 
     if(value ==="="){
-
+      this.calculateResult();
       return;
     }
 
@@ -34,7 +34,9 @@ export class CalculatorService {
 
     if(value ==="Backspace"){
       if(this.resultText() === "0"){
+        this.lastOperator.set("");
       } else if(this.resultText().length <= 1){
+        this.lastOperator.set("");
         this.resultText.set("0");
       }else{
         this.resultText.update(prev => prev.slice(0, -1));
@@ -43,7 +45,8 @@ export class CalculatorService {
       return;
     }
 
-    if(operators.includes(value)){
+    if(operators.includes(value) || value === "%"){
+      this.calculateResult();
       this.lastOperator.set(value);
       this.subResultText.set(this.resultText());
       this.resultText.set("0");
@@ -73,6 +76,42 @@ export class CalculatorService {
     }else{
       this.resultText.update(prev => prev + value);
     }
-
   }
+
+  public calculateResult(){
+    const number1 = parseFloat(this.subResultText());
+    const number2 = parseFloat(this.resultText());
+    let result= 0;
+
+    if(number1 === 0){
+      return;
+    }
+
+    switch(this.lastOperator()){
+      case "+":
+        result = number1 + number2;
+        break;
+
+      case "-":
+        result = number1 - number2;
+        break;
+
+      case "x":
+        result = number1 * number2;
+        break;
+
+      case "÷":
+        result = number1/number2;
+        break;
+      case "%":
+        result = number1*(number2/100);
+        break;
+    }
+
+    this.resultText.set(result.toString());
+    this.subResultText.set("0");
+    this.lastOperator.set("");
+  }
+
+
 }
